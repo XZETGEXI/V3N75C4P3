431a752fad09a9ea457bc1b818e1caf42a6c3f58
XZETGEXI <77068772+XZETGEXI@users.noreply.github.com>
2021-01-28 16:55:09 +0100

    Update venter.py
    Wait before requests so the API can breath.
    If the API sends "Too many requests" error anyway, pass.

--- venter.py
+++ venter.py
@@ -4,11 +4,13 @@ W, H = os.get_terminal_size()
 
 FILTER = True
 PATTERN = r"^(\d+)$"
+API_COOLDOWN = 1
 
 async def retrieve(to_print, printed):
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.ventspace.life/api/v1/ventpost/recent') as response:
             recents = await response.json()
+            await asyncio.sleep(API_COOLDOWN)
     
     for msg in [recent["messageText"] for recent in recents]:
         if msg not in printed and not re.match(r"^(\d+)$", msg):
@@ -22,8 +24,11 @@ async def print_msg(to_print, printed):
         await asyncio.sleep(1)
     
 async def venter(to_print, printed):
-    await retrieve(to_print, printed)
-    await print_msg(to_print, printed)
+    try:
+        await retrieve(to_print, printed)
+        await print_msg(to_print, printed)
+    except Exception as e:
+        pass
 
 def main():
     to_print = []
